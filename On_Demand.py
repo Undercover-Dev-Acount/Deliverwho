30cfc54732495b92a0d24f6e7e4c7bba8a66220d
Rob Gooderson <robert.gooderson@sainsburys.co.uk>
2022-05-16 12:01:11 +0100

    First version of all files

---
+++ On_Demand.py
@@ -0,0 +1,72 @@
+## Functions
+import printfunc
+
+## Product names
+soft_drinks = ["Lemonade", " Coke", " Orange Juice", " Water", 'Dr Pepper', 'Fanta', 'Lucozade']
+adult_drinks = ["Vodka Lemonade", " Rum and Coke", " Pint", " House Red", " House White"]
+sandwiches = ['BLT', ' Egg and Cress', ' Chicken and Stuffing', ' Tuna Sweetcorn', ' Ham and Cheese', ' Smoked Salmon and Cream Cheese']
+snacks = ['Cheese and Onion Crisps', 'Ready Salted Crisps', 'Prawn Cocktail Crisps', 'McCoys Steak Crisps', 'Sour Cream Pringles']
+confec = ['KitKat 4', 'KitKat Chunky', 'Snickers', 'Mars Bar', 'Lion Bar', 'Kinder Bueno', 'Twix', 'Chocolate Chip Cookie', 'Sugared Ring Donut']
+
+## Main Menu
+print('Welcome to \033[1m Deliverwho? \033[0m')
+print('Please use the terminal to select an option below:\n')
+
+exitloop = False
+while exitloop == False:
+    print('\n''\033[1m'
+    '1) Inventory Management \n'
+    '2) Version Information\n'
+    '0) Exit' '\033[0m')
+
+    choice = input()
+    if choice == '1':
+        print('Please use the terminal to browse the menus:\n'
+        '\n''\033[1m'
+        '1) Sandwiches \n'
+        '2) Snacks\n'
+        '3) Soft Drinks\n'
+        '4) Alcohol\n'
+        '5) Confectionary\n'
+        '0) Exit' '\033[0m')
+        
+        choice =input('Please enter your desired number below \n')
+        if choice == '1':
+            print(*sandwiches, sep = ',')
+            
+            printfunc.item_options(sandwiches)
+        
+        elif choice =='2':
+            print(*snacks, sep = ',')
+
+            printfunc.item_options(snacks)
+        elif choice == '3':
+            print(*soft_drinks, sep = ',')
+
+            printfunc.item_options(soft_drinks)
+        elif choice == '4':
+            
+            age = int(input(f'How old are you?\n'))
+            if age < 18:
+                continue
+            
+            print(*adult_drinks, sep = ',')
+
+            printfunc.item_options(adult_drinks)
+        elif choice == '5':
+            print(*confec, sep = ',')
+
+            printfunc.item_options(confec)
+        elif choice == '0':
+            continue
+        
+    elif choice == '2':
+        print('Deliverwho v.0.0.1')
+        input('Press enter to return to main menu')
+    elif choice == '0':
+        exitloop = True
+        exit()
+       
+        
+
+
---
+++ printfunc.py
@@ -0,0 +1,24 @@
+def print_list(list):
+        index = 0
+        for x in list:
+            print(index, x)
+            index += 1
+
+def item_options(list):
+
+    print('Options:\n1) Add \n2) Edit \n3) Delete\n4) Return to Main Menu')
+    choice = input()
+    if choice == '1':
+            new_entry =input('please type the product name you would like to add\n')
+            list.append(new_entry)
+        
+    elif choice == '2':
+           
+            print_list(list)
+            edit_entry = int(input('Select an item number to edit\n'))
+            
+            list[edit_entry] = input("Enter new item here\n")
+    elif choice == '3':
+            print_list(list)
+            del_entry = int(input('Select an item number to delete\n'))
+            list.remove(del_entry)
