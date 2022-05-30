9bf4139ca37d8edb67fe926745cc1a833769bd10
Rob Gooderson <robert.gooderson@sainsburys.co.uk>
2022-05-30 09:23:40 +0100

    The start of my products module

---
+++ src/products.py
@@ -0,0 +1,42 @@
+
+
+"""This module is a collection of product classes which will allow easy creation and editing of items with their associated prices"""
+
+
+## Imports
+
+
+
+
+## Classes 
+
+class Product:
+
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = ()
+
+## Method
+    def multiple(self):
+        amount = int(input())
+        self.price *= amount    
+    
+class Sandwich(Product):
+    def __init__(self, name: str, price: float):
+        super().__init__(name, price)
+
+class Soft_drinks(Product):
+    def __init__(self, name: str, price: float, size: int):
+        super().__init__(name, price)
+
+class Snacks(Product):
+    def __init__(self, name: str, price: float):
+        super().__init__(name, price)
+
+class Adult_drinks(Product):
+    def __init__(self, name: str, price: float):
+        super().__init__(name, price)
+
+class Confec(Product):
+    def __init__(self, name: str, price: float):
+        super().__init__(name, price)
