c13b85204b54d17dd66799c1da4bf512078e84f0
Rob Gooderson <robert.gooderson@sainsburys.co.uk>
2022-06-06 16:46:48 +0100

    fixed some tests and some refactoring

--- src/test_age.py
+++ src/test_age.py
@@ -1,26 +1,24 @@
 
-from functions import age_check
+from My_Functions.functions import age_check
 from unittest.mock import patch
 
 @patch('builtins.input')
 def test_age_check(mock_input):
     mock_input.return_value = 18
-    age = input
     min_age = 18
 
     expected = True 
-    actual = age_check(age, min_age)
+    actual = age_check(mock_input, min_age)
 
     assert expected == actual
 
 @patch('builtins.input')
 def test_under_age_check(mock_input):
     mock_input.return_value = 17
-    age = input
     min_age = 18
 
     expected = False
-    actual = age_check(age, min_age)
+    actual = age_check(mock_input, min_age)
 
     assert expected == actual
 
@@ -28,10 +26,9 @@ def test_under_age_check(mock_input):
 @patch('builtins.input')
 def test_not_int_check(mock_input):
     mock_input.return_value = 'what?'
-    age = input
     min_age = 18
 
     expected = False
-    actual = age_check(age, min_age)
+    actual = age_check(mock_input, min_age)
 
     assert expected == actual
