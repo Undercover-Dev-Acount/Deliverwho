8eacdd42e085bb7f0e23c1e40d2b93698670a60d
Rob Gooderson <robert.gooderson@sainsburys.co.uk>
2022-06-06 15:21:07 +0100

    another test

---
+++ src/test_func.py
@@ -0,0 +1,11 @@
+from functions import print_list
+from unittest.mock import patch
+
+
+@patch('builtins.print')
+def test_print_list(mock_print):
+        list = ['tests', 'tests', 'more tests']
+        print_list(list)
+        mock_print.assert_called_with(2, 'more tests')
+        mock_print.assert_any_call(0, 'tests')
+        mock_print.assert_any_call(1, 'tests')
